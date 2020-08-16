0f6149cef7badf201b42c757bb1228413617c6e4
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-15 20:55:05 -0400

    apiroutes.js get post and delete method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,52 @@
-const { fstat } = require("fs");
+var db = require("../db/db.json");
+var fs = require("fs");
 
-let db = []
-
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        fs.readFiles(".db/db.json","utf8", (err,data) => {
+module.exports = function (app) {
+    // get
+    app.get("/api/notes", function (req, res){
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
             if (err) throw err;
             const db = JSON.parse(data);
             res.json(db);
-      });
-    app.post("/api/notes", function(req, res) {
-        res.json(tableData);
+        
+        });
     });
-    app.delete("/api/notes", function(req, res) {
-        res.json(tableData);
+// post
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        const db = JSON.parse(data);
+        const newDB = [];
+        db.push(req.body);
+        for (let i = 0; i < db.length; i++) {
+            const newNote = {
+                id: i,
+                title: db[i].title,
+                text: db[i].text,
+            };
+            newDB.push(newNote);
+        }
+        fs.writeFile("./db/db.json", JSON.stringify(newDB, null, 2), (err) => {
+            if (err) throw err;
+            res.json(newDB);
+            console.log("New Note Created");
+        });
     });
+});
+//delete
+app.delete("/api/notes/:id", (req, res) =>{
+    let idNote = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) =>{
+        if (err) throw err;
+        
+    const notes = JSON.parse(data);
+    const newNotes = notes.filter(note => note.id != idNote);
+    fs.writeFile("./db/db.json", JSON.stringify(newNotes, null, 2), err => {
+        if (err) throw err;
+        res.send(db);
+        console.log("Deleted!")
     });
+});
+});
 }
